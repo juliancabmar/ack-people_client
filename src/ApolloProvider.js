e7227eac08c89ea9af31f6bf786cd3a5b71b4a17
Julian Cabrera <a.j.c.marce@gmail.com>
2022-09-16 05:48:09 -0300

    branch to_vercel created

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:8080'
+  uri: 'https://ack-people-server.vercel.app/'
 });
 
 const authLink = setContext(() => {
