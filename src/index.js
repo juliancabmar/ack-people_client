40e55e03a217778528b6eb9da53fc423f62451f9
Julian Cabrera <a.j.c.marce@gmail.com>
2022-10-03 18:46:56 -0300

    changing tab head title

--- src/index.js
+++ src/index.js
@@ -3,5 +3,3 @@ import * as serviceWorker from './serviceWorker';
 import ApolloProvider from './ApolloProvider';
 
 ReactDOM.render(ApolloProvider, document.getElementById('root'));
-
-//serviceWorker.unregister();
